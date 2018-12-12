4bf2e0ad667331a88fd7a09c64b91c610efeb715
Balwant Singh <balwant.matharu@gmail.com>
2018-12-12 14:59:15 +1100

    You can kill monsters now

---
+++ assets/index.js
@@ -0,0 +1,13 @@
+const assets_dir = __dirname;
+
+module.exports = {
+  LOG: "",
+  FPS: 100,
+  IMAGES: {
+    SPACESHIP: "🔺",
+    SPACESHIP_FIRE: "🔅",
+    SPACESHIP_FIRE_BLAST: "🎇",
+    SPACESHIP_FIRE_BLAST_END: "🔥",
+    MONSTER: "👾"
+  }
+};
--- cli.js
+++ cli.js
@@ -1,3 +1,78 @@
 #! /usr/bin/env node
+const termkit = require("terminal-kit");
+const term = termkit.terminal;
+const ScreenBuffer = termkit.ScreenBuffer;
 
-console.log("Starting Galaxian Game . . .");
+const Assets = require("./assets");
+const SpaceShip = require("./lib/spaceship");
+const Bullets = require("./lib/bullets");
+const Monsters = require("./lib/monsters");
+
+var screen = new ScreenBuffer({ dst: term, noFill: true });
+const ship = new SpaceShip(screen);
+const monsters = new Monsters(screen);
+const bullets = new Bullets(screen, monsters);
+monsters.generate();
+
+term.on("key", function(name, matches, data) {
+  if (name === "CTRL_C") {
+    term.grabInput(false);
+    screen.clear();
+    setTimeout(function() {
+      process.exit();
+    }, 100);
+  } else {
+    // console.log("Key: ", name);
+    switch (name) {
+      case "LEFT":
+        ship.goLeft();
+        break;
+      case "RIGHT":
+        ship.goRight();
+        break;
+      case " ":
+        bullets.fire(ship);
+        break;
+      default:
+        break;
+    }
+  }
+  screen.put(
+    {
+      x: 1,
+      y: 1
+    },
+    `${name} ${ship.location.x}        `
+  );
+});
+
+term.grabInput(true);
+
+console.log(
+  "Starting Galaxian Game . . .",
+  Assets.IMAGES.SPACESHIP,
+  Assets.IMAGES.MONSTER,
+  term.width,
+  term.height
+);
+
+screen.fill({
+  attr: {
+    // Both foreground and background must have the same color
+    color: 0,
+    bgColor: 0
+  }
+});
+
+// 10 FPS refresh rate
+setInterval(() => {
+  // monsters.renderStats();
+  screen.put({ x: 0, y: 0 }, `Score ${bullets.score}`);
+  screen.put({ x: term.width - 10, y: 0 }, `Level ${bullets.level}`);
+  screen.put(
+    { x: 0, y: term.height - 1 },
+    `Kill ${monsters.Monsters.length} Monsters`
+  );
+  // screen.put({ x: 0, y: term.height - 1 }, Assets.LOG);
+  screen.draw();
+}, Assets.FPS);
